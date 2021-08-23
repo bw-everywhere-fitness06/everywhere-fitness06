257fbe159dbc6f13144f6dba7cb43d8776fd5cdd
Jie Zhang <zhangjiew@gmail.com>
2021-08-23 10:56:36 -0600

    started form

--- client/src/components/CreateClassForm.js
+++ client/src/components/CreateClassForm.js
@@ -1,5 +1,34 @@
+import React from "react";
+import { useState } from "react";
+
 function CreateClassForm() {
-  return <div>CreateClassForm</div>;
+  const initialClass = {
+    name: "",
+    type: "",
+    startTime: "",
+    duration: "",
+    intensityLevel: "",
+    location: "",
+    maxSize: "",
+    currentEnrolled: "",
+  };
+
+  const [inputClass, setInputClass] = useState(initialClass);
+  console.log(inputClass);
+  return (
+    <form>
+      <div>
+        <label htmlFor="name">Event Name</label>
+        <input
+          type="text"
+          name="name"
+          onChange={(e) =>
+            setInputClass({ ...setInputClass, name: e.target.value })
+          }
+        />
+      </div>
+    </form>
+  );
 }
 
 export default CreateClassForm;
