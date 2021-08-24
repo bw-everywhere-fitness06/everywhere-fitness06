ab519711a94973ba7e1a19fc7ab4ab8e87a826e8
Raj Patel <rajpatel@Rajs-MacBook-Pro.local>
2021-08-24 11:46:41 -0400

    Account Page and some stylings

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,38 @@
 import "./App.css";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-import Header from "./components/Header";
+// import Header from "./components/Header";
 import SignIn from "./components/SignIn";
 import SignUp from "./components/SignUp";
 import Home from "./components/Home";
 import Classes from "./components/Classes";
+import styled from 'styled-components';
+import { AccountBox } from './components/accountBox'
+
+const AppContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`
 
 function App() {
   return (
     <BrowserRouter>
-      <Header />
-      <Route exact path="/" component={Home} />
+
+    <AppContainer>
+    
+  
+      <Route exact path="/" component={Home} >
+      <AccountBox />
+      </Route>
       <Route path="/signin" component={SignIn} />
       <Route path="/signup" component={SignUp} />
       <Route path="/classes" component={Classes} />
       {/* <Route path="/" component={Home} />
       <Route path="/" component={Home} /> */}
+        </AppContainer>
     </BrowserRouter>
   );
 }
