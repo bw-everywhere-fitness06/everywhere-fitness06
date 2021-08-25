5d21b8fb7ceb1c598e35f61d2afc38f26445d1c2
Jie Zhang <zhangjiew@gmail.com>
2021-08-25 09:51:14 -0600

    fixed server

--- server/index.js
+++ server/index.js
@@ -2,12 +2,12 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
-
+import classesRoutes from "./routes/classes.js";
 const app = express();
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
-
+app.use("/classes", classesRoutes);
 const CONNECTION_URL =
   "mongodb+srv://jiechelchel:jiechelchel427628@cluster0.ta3zy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 
