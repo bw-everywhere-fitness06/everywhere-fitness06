776e91870a43a98af17c506813dfe5d43935982c
Andrewschaer <andrewschaer@github.com>
2021-08-25 19:28:49 -0400

    removed old server

--- server/routes/classes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { getClasses, createClass, updateClass } from "../controllers/posts.js";
-
-const router = express.Router();
-
-router.get("/", getClasses);
-router.post("/", createClass);
-router.patch("/:id", updateClass);
-export default router;
